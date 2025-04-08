e7e26577acc0a944b20f53b4a81610aa15163179
Alex Chenet <chenet.alex@gmail.com>
2025-04-08 16:21:21 -0400

    code 5 added JSON

--- Project/5_most_optimal_skills.sql
+++ Project/5_most_optimal_skills.sql
@@ -28,3 +28,158 @@ ORDER BY
 	avg_salary DESC,
 	demand_count DESC
 LIMIT 25;
+
+/*
+[
+  {
+    "skill_id": 8,
+    "skills": "go",
+    "demand_count": "27",
+    "avg_salary": "115319.89"
+  },
+  {
+    "skill_id": 97,
+    "skills": "hadoop",
+    "demand_count": "22",
+    "avg_salary": "113192.57"
+  },
+  {
+    "skill_id": 80,
+    "skills": "snowflake",
+    "demand_count": "37",
+    "avg_salary": "112947.97"
+  },
+  {
+    "skill_id": 74,
+    "skills": "azure",
+    "demand_count": "34",
+    "avg_salary": "111225.10"
+  },
+  {
+    "skill_id": 76,
+    "skills": "aws",
+    "demand_count": "32",
+    "avg_salary": "108317.30"
+  },
+  {
+    "skill_id": 4,
+    "skills": "java",
+    "demand_count": "17",
+    "avg_salary": "106906.44"
+  },
+  {
+    "skill_id": 233,
+    "skills": "jira",
+    "demand_count": "20",
+    "avg_salary": "104917.90"
+  },
+  {
+    "skill_id": 79,
+    "skills": "oracle",
+    "demand_count": "37",
+    "avg_salary": "104533.70"
+  },
+  {
+    "skill_id": 185,
+    "skills": "looker",
+    "demand_count": "49",
+    "avg_salary": "103795.30"
+  },
+  {
+    "skill_id": 1,
+    "skills": "python",
+    "demand_count": "236",
+    "avg_salary": "101397.22"
+  },
+  {
+    "skill_id": 5,
+    "skills": "r",
+    "demand_count": "148",
+    "avg_salary": "100498.77"
+  },
+  {
+    "skill_id": 78,
+    "skills": "redshift",
+    "demand_count": "16",
+    "avg_salary": "99936.44"
+  },
+  {
+    "skill_id": 182,
+    "skills": "tableau",
+    "demand_count": "230",
+    "avg_salary": "99287.65"
+  },
+  {
+    "skill_id": 186,
+    "skills": "sas",
+    "demand_count": "63",
+    "avg_salary": "98902.37"
+  },
+  {
+    "skill_id": 7,
+    "skills": "sas",
+    "demand_count": "63",
+    "avg_salary": "98902.37"
+  },
+  {
+    "skill_id": 61,
+    "skills": "sql server",
+    "demand_count": "35",
+    "avg_salary": "97785.73"
+  },
+  {
+    "skill_id": 9,
+    "skills": "javascript",
+    "demand_count": "20",
+    "avg_salary": "97587.00"
+  },
+  {
+    "skill_id": 183,
+    "skills": "power bi",
+    "demand_count": "110",
+    "avg_salary": "97431.30"
+  },
+  {
+    "skill_id": 0,
+    "skills": "sql",
+    "demand_count": "398",
+    "avg_salary": "97237.16"
+  },
+  {
+    "skill_id": 215,
+    "skills": "flow",
+    "demand_count": "28",
+    "avg_salary": "97200.00"
+  },
+  {
+    "skill_id": 201,
+    "skills": "alteryx",
+    "demand_count": "17",
+    "avg_salary": "94144.53"
+  },
+  {
+    "skill_id": 199,
+    "skills": "spss",
+    "demand_count": "24",
+    "avg_salary": "92169.68"
+  },
+  {
+    "skill_id": 22,
+    "skills": "vba",
+    "demand_count": "24",
+    "avg_salary": "88783.29"
+  },
+  {
+    "skill_id": 196,
+    "skills": "powerpoint",
+    "demand_count": "58",
+    "avg_salary": "88701.09"
+  },
+  {
+    "skill_id": 181,
+    "skills": "excel",
+    "demand_count": "256",
+    "avg_salary": "87288.21"
+  }
+]
+*/
